267e57af62bacc7dad0b3b430793fecb6dd74e01
Malory Rose <maloryrose07@outlook.com>
2025-01-31 15:41:43 -0800

    update process scripts

--- infra/scripts/run_create_index_scripts.sh
+++ infra/scripts/run_create_index_scripts.sh
@@ -10,7 +10,9 @@ requirementFileUrl=${baseUrl}"infra/scripts/index_scripts/requirements.txt"
 echo "Script Started"
 
 # Download the create_index and create table python files
-curl --output "process_data.py" ${baseUrl}"infra/scripts/index_scripts/process_data.py"
+curl --output "01_create_search_index.py" ${baseUrl}"infra/scripts/index_scripts/01_create_search_index.py"
+curl --output "02_create_cu_template_text.py" ${baseUrl}"infra/scripts/index_scripts/02_create_cu_template_text.py"
+curl --output "03_cu_process_data_text.py" ${baseUrl}"infra/scripts/index_scripts/03_cu_process_data_text.py"
 
 # RUN apt-get update
 # RUN apt-get install python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
@@ -25,8 +27,12 @@ curl --output "$requirementFile" "$requirementFileUrl"
 echo "Download completed"
 
 #Replace key vault name 
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "process_data.py"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "01_create_search_index.py"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "02_create_cu_template_text.py"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "03_cu_process_data_text.py"
 
 pip install -r requirements.txt
 
-python process_data.py
+python 01_create_search_index.py
+python 02_create_cu_template_text.py
+python 03_cu_process_data_text.py
